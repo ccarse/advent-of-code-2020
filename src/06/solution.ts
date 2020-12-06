d751fefc55b3daf984206a4bdcaebc429e2cf8fe
Cody Carse <ccarse@gmail.com>
2020-12-06 12:20:45 -0500

    Day 6

--- src/06/solution.ts
+++ src/06/solution.ts
@@ -1,10 +1,14 @@
 import fs from 'fs';
-
-const inputText = fs.readFileSync('./src/06/input.txt', 'utf-8').trim();
-const lines = inputText.split('\n');
+import _ from 'lodash'; 
 
 // Part 1
-console.log(`Part 1: ${lines}`);
+const groups = fs.readFileSync('./src/06/input.txt', 'utf-8').split('\n\n');
+console.log(`Part 1: ${groups.map(group => new Set(group.split("\n").join("")).size ).reduce((a, b) => a + b, 0)}`);
 
 // Part 2
-console.log(`Part 2: ${lines}`);
+const sums = groups.map(group => {
+  const answers = group.split("\n").map(a => a.split(""));
+  return _.intersection(...answers).length;
+});
+
+console.log(`Part 1: ${sums.reduce((a, b) => a + b, 0)}`);
