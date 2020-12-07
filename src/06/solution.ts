04cc07e6bfde4ff0dea5003c943c2a53978d173f
Cody Carse <cody.carse@upstart.com>
2020-12-07 12:03:19 -0500

    Day 7. Meh

--- src/06/solution.ts
+++ src/06/solution.ts
@@ -1,13 +1,13 @@
 import fs from 'fs';
-import _ from 'lodash'; 
+import _ from 'lodash';
 
 // Part 1
 const groups = fs.readFileSync('./src/06/input.txt', 'utf-8').split('\n\n');
-console.log(`Part 1: ${groups.map(group => new Set(group.split("\n").join("")).size ).reduce((a, b) => a + b, 0)}`);
+console.log(`Part 1: ${groups.map(group => new Set(group.split('\n').join('')).size).reduce((a, b) => a + b, 0)}`);
 
 // Part 2
 const sums = groups.map(group => {
-  const answers = group.split("\n").map(a => a.split(""));
+  const answers = group.split('\n').map(a => a.split(''));
   return _.intersection(...answers).length;
 });
 
--- src/07/solution.ts
+++ src/07/solution.ts
@@ -1,10 +1,58 @@
 import fs from 'fs';
+import _ from 'lodash';
+
+// This day sucked. This solution sucks. Idc. I'm done with this day.
 
 const inputText = fs.readFileSync('./src/07/input.txt', 'utf-8').trim();
-const lines = inputText.split('\n');
+const rules = inputText.split('\n').map(rule => {
+  const [container, contents] = rule.split(' bags contain ');
+  return {
+    container,
+    contents
+  };
+});
+
+const findContainingBags = (colors: Set<string>) => {
+  let containingBags = new Set<string>(colors);
+  for (const rule of rules) {
+    if ([...colors].filter(color => rule.contents.includes(color)).length > 0) {
+      containingBags.add(rule.container);
+    };
+  }
+  if (!_.isEqual(colors, containingBags)) {
+    containingBags = findContainingBags(containingBags);
+  }
+  return containingBags;
+}
+
 
 // Part 1
-console.log(`Part 1: ${lines}`);
+const answer = findContainingBags(new Set(['shiny gold']));
+answer.delete('shiny gold');
+console.log(`Part 1: ${answer.size}`);
 
 // Part 2
-console.log(`Part 2: ${lines}`);
+const newRules = Object.fromEntries(inputText.split('\n').map(rule => {
+  const [container, contents] = rule.split(' bags contain ');
+  const ruleRules = contents === 'no other bags.' ? [] : contents.split(', ').map(c => {
+    return [c.split(' ').slice(1, 3).join(' '), parseInt(c.split(' ')[0], 10)];
+  });
+  return [
+    container,
+    Object.fromEntries(ruleRules)
+  ];
+}));
+
+const numBags: number[] = [];
+const findBags = (color: string, multiplier: number) => {
+  const containerBags = newRules[color];
+  Object.entries(containerBags).forEach(([key, element]) => {
+    numBags.push((element as number) * multiplier);
+    // console.log(numBags);
+    findBags(key, (element as number) * multiplier);
+  });
+}
+
+findBags('shiny gold', 1);
+
+console.log(`Part 2: ${numBags.reduce((a, b) => a + b, 0)}`);
